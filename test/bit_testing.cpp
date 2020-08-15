9e3cff9a68277a419ff2eaf4a27725959f605670
Nicholas Steele <nichlock@gmail.com>
2020-08-15 11:07:31 -0400

    Create bit testing for seperated testing of board_interface.

---
+++ test/bit_testing.cpp
@@ -0,0 +1,6 @@
+#include "bit_testing.h"
+
+int main(int argc, char const *argv[]) {
+	printf("Bit test starting: %s", val);
+	return 0;
+} // main
---
+++ test/bit_testing.h
@@ -0,0 +1,8 @@
+#ifndef BIT_TESTING_H
+#define BIT_TESTING_H
+
+#include <stdio.h>
+
+const static char *val = "testing...";
+
+#endif /* end of include guard: BIT_TESTING_H */
