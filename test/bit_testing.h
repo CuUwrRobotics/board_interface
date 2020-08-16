466d1628b257d1b0448a51909a7601282ba6a2b0
Nicholas Steele <nichlock@gmail.com>
2020-08-16 15:35:35 -0400

    Moved include files so that they are common between test and node

--- test/bit_testing.h
+++ test/bit_testing.h
@@ -1,7 +1,11 @@
 #ifndef BIT_TESTING_H
 #define BIT_TESTING_H
 
+#include <stdint.h>
 #include <stdio.h>
+#include <ros/ros.h>
+#include "Logger.h"
+#include "HardwareMessages.h"
 
 const static char *val = "testing...";
 
